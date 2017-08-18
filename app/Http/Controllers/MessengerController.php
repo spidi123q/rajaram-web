877a74cceb9c643fac240b70e5602e16b2a31aae
spidi123q <spidi123q@gmail.com>
2017-08-18 19:59:29 +0530

    first

---
+++ app/Http/Controllers/MessengerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessengerController extends Controller
+{
+    function verify(Request $request){
+        if($request->get('hub_verify_token') === 'rajaram'){
+            echo $request->get('hub_challenge');
+        }
+        else{
+            echo 'invalid access';
+        }
+    }
+}
