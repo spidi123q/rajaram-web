25a547ae30f110006ac386a83c568c89f82bb97a
spidi123q <spidi123q@gmail.com>
2017-08-25 21:38:01 +0530

    route is now api

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 class MessengerController extends Controller
 {
     function verify(Request $request){
+
         if($request->get('hub_verify_token') === 'rajaram'){
             echo $request->get('hub_challenge');
         }
         else{
             echo 'Error : invalid access';
         }
+    }
+
+    function setClientUrl(Request $request){
+        session(['CLIENT_URL' => $request->get('url')]);
+        return session('CLIENT_URL');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/messenger','MessengerController@verify');
 
