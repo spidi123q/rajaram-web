402d4bf6f0652e33ade42e67a5bf308124c6e438
spidi123q <spidi123q@gmail.com>
2017-08-26 15:01:18 +0530

    forward fb message to robot

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class MessengerController extends Controller
@@ -19,5 +20,14 @@ class MessengerController extends Controller
     function setClientUrl(Request $request){
         session(['CLIENT_URL' => $request->get('url')]);
         return ['url' => session('CLIENT_URL')];
+    }
+
+    function onMessage(Request $request){
+
+        $client = new Client();
+        $response = $client->request('POST', session('CLIENT_URL').'/messenger', [
+            'json' => json_encode($request->all())
+        ]);
+        return $response->getBody();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/messenger','MessengerController@verify');
+Route::post('/messenger','MessengerController@onMessage');
 Route::get('/setClientUrl','MessengerController@setClientUrl');
